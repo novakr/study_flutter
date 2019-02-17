8f542f86d8c2cd369184fc6231385ce1aa46ae31
zengjr <jr.zengjr@gmail.com>
2019-02-17 21:39:05 +0700

    使用界面组件与定制界面主题

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-  runApp(
-    App()
-  );
+void main() => runApp(App());
 
 
 class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ZXY'),
+          elevation: 0.0,
+           
+        ),
+        body: Hello(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow
+      ),
+    );
     
+  }
+
+}
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Center(child: Text(
       'hello Flutter Widget',
       textDirection:TextDirection.ltr,
       style: TextStyle(
         fontSize: 30.0,
         fontWeight: FontWeight.bold,
-        color: Colors.yellow
+        color: Colors.blue
       ),
     ),);
   }
-
 }
