13f96a8d296711c56b05a099f48710ad35b77f1d
zengjr <jr.zengjr@gmail.com>
2019-02-17 22:13:19 +0700

    列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'model/post.dart';
 
 void main() => runApp(App());
 
@@ -10,10 +11,10 @@ class App extends StatelessWidget{
       home: Scaffold(
         appBar: AppBar(
           title: Text('ZXY'),
-          elevation: 0.0,
+          elevation: 10.0,
            
         ),
-        body: Hello(),
+        body: Home(),
       ),
       theme: ThemeData(
         primarySwatch: Colors.yellow
@@ -23,6 +24,48 @@ class App extends StatelessWidget{
   }
 
 }
+
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context,int index){
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0,),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 16.0),
+        ],
+      ),
+      );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Text('ZXY'),
+          elevation: 10.0,
+           
+        ),
+        body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: _listItemBuilder,
+        )
+      );
+  }
+}
+
+
+
 class Hello extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
---
+++ lib/model/post.dart
@@ -0,0 +1,38 @@
+class Post{
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl
+  });
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+final List<Post> posts= [
+  Post(
+    title:'1',
+    author: '1',
+    imageUrl: 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=2429172212,2430577988&fm=173&app=49&f=JPEG?w=640&h=809&s=141915DF227B6794E0AD19B803007040'
+    ),
+     Post(
+    title:'2',
+    author: '2',
+    imageUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1737876561,2561720235&fm=173&app=49&f=JPEG?w=640&h=829&s=D0A7BD54E8A3529C63A8256903008062'
+    ),
+     Post(
+    title:'3',
+    author: '3',
+    imageUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1900706268,1943820733&fm=173&app=49&f=JPEG?w=640&h=456&s=5A23A944FCCA11559C79F5870300E081'
+    ),
+     Post(
+    title:'4',
+    author: '4',
+    imageUrl: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=72120892,1299090365&fm=173&app=49&f=JPEG?w=640&h=415&s=7664D94A465A027E404234380300C018'
+    ),
+     Post(
+    title:'5',
+    author: '5',
+    imageUrl: 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=1785095423,2319993314&fm=173&app=49&f=JPEG?w=640&h=463&s=7F622DC502732F9C5819C10B030040C3'
+    )
+    
+]
