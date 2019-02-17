d627203208c0190ffc23cea3e67d7b91c6337c74
zengjr <jr.zengjr@gmail.com>
2019-02-17 21:14:49 +0700

    statelesswidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() =>
   runApp(
-    Center(child: Text(
-      'hello Flutter',
-      textDirection:TextDirection.ltr
-    ),)
+    App()
   );
+
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    
+    return Center(child: Text(
+      'hello Flutter Widget',
+      textDirection:TextDirection.ltr,
+      style: TextStyle(
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.yellow
+      ),
+    ),);
+  }
+
 }
-
-
